58f87e461eab8d2fa44a0e54699abcc7f2d8e2bd
Sergey Titov <sergeyntitov@gmail.com>
2023-12-27 22:27:44 +0200

    base commit chapter 1,2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-updated-max-schwarzmuller';
+  name: string = '';
+  position: string = 'Admin';
+  myForm!: NgForm
+  serverName: string= '';
+  flag: boolean = false;
+
+
+
+  onSubmit(data: any){
+    console.log(data);
+    this.name = data
+    this.name = ''
+  }
+
+  onServerCreated(data: string) {
+    this.serverName = data
+    this.flag = true
+
+  }
+
+  onResetServer(data: any) {
+    this.serverName = ''
+    this.flag = false
+
+  }
 }
