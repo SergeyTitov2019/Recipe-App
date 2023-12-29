36fdbf3d6acc7ebee8caf59baafb5898a15e0ea5
Sergey Titov <sergeyntitov@gmail.com>
2023-12-29 23:30:53 +0200

    base commit chapter 74

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {FormGroup, NgForm} from "@angular/forms";
+import { ServerElements } from "./types/server-elements";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
   title = 'ang-updated-max-schwarzmuller';
   name: string = '';
@@ -14,6 +18,42 @@ export class AppComponent {
   serverName: string= '';
   flag: boolean = false;
 
+  serverElements:ServerElements[] = [
+    {type: 'server', name: 'Test server', content: 'Just a content'},
+    {type: 'blueprint', name: 'Test blueprint', content: 'Just a blueprint content'},
+    ]
+
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    console.log(serverData)
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    })
+  }
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    })
+  }
+  //
+  // onAddServer() {
+  //   this.serverElements.push({
+  //     type: 'server',
+  //     name: this.newServerName,
+  //     content: this.newServerContent
+  //   })
+  // }
+  // onAddBlueprint() {
+  //   this.serverElements.push({
+  //     type: 'blueprint',
+  //     name: this.newServerName,
+  //     content: this.newServerContent
+  //   })
+  // }
 
 
   onSubmit(data: any){
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
+import { HeaderComponent } from './components/header/header.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './components/recipes/recipes-detail/recipes-detail.component';
+import { RecipesItemComponent } from './components/recipes/recipes-list/recipes-item/recipes-item.component';
+import { ShoppingListComponent } from './components/shoping-list/shopping-list.component';
+import { ShoppingEditComponent } from './components/shoping-list/shopping-edit/shopping-edit.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailComponent,
+    RecipesItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
